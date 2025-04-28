73efba40e21f4fa88faa6247e2b51f68ab7dddea
surendrareddy235 <157972541+surendrareddy235@users.noreply.github.com>
2025-04-28 04:15:42 -0700

    Delete project_selling directory

--- project_selling/model.py
+++
@@ -1,6 +0,0 @@
-from db import db
-
-class Signup(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
